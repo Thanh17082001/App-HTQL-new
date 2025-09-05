7be11b3ccd73337a0a5044fba06cf7830c1886d9
Thanh17082001 <thienthanh17082001@gmail.com>
2025-09-05 11:13:43 +0700

    first commit 3

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     GMSServices.provideAPIKey("AIzaSyCmYZvHyila52BYEXk0uLwDcviwu2l3xm4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
